8fe7e788ae4b2c5a95d69aeaeec198fc4951aced
Kopamed <kopamedve@gmail.com>
2021-03-28 23:43:35 +0200

    So i found outr how to get the people that reacted using a reaction

--- alt.py
+++ alt.py
@@ -3,16 +3,17 @@ import os
 from dotenv import load_dotenv
 from core import *
 import keep_alive
+import random
 
 
 
 
-PREFIX = "/"
+PREFIX = "f."
 PREFIX_LEN = len(PREFIX)
 
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('token')
 
 
 client = discord.Client()
@@ -26,3 +27,16 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    if len(message.content) >= 2 and message.content[0:2] == "f.":
+        if len(message.content.split()) == 2:
+            message_id = message.content.split()[1]
+            dynos_message = await message.channel.fetch_message(message_id)
+            print(dynos_message.reactions)
+            users = await dynos_message.reactions[0].users().flatten()
+            # users is now a list of User...
+            winner = random.choice(users)
+            await message.channel.send('{} has won the raffle.'.format(winner))
+        
+
+
+client.run(TOKEN)
